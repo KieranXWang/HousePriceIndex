30cf81d01fc30167cb166a14972fe1b32768a04b
KieranXWang <kxw@bu.edu>
2023-10-01 21:24:45 -0700

    add latest_update file option

--- src/data_builder.py
+++ src/data_builder.py
@@ -4,7 +4,7 @@ import time
 import csv
 from tqdm import tqdm
 
-from src.utils import get_project_root
+from src.utils import get_project_root, convert_timestamp_to_readable_format
 from src.redfin_utils import get_current_price
 
 
@@ -13,13 +13,15 @@ root = get_project_root()
 
 class DataBuilder:
     def __init__(self, realty_database_path: str = root + 'realty_database/seattle.csv',
-                 price_database_path: str = root + 'data/weekly/price_data.txt'):
+                 price_database_path: str = root + 'data/weekly/price_data.txt',
+                 latest_update_path: str = ''):
         # timestamp
         self.timestamp = int(time.time())  # 10-digit epoch time
 
         # settings
         self.realty_database_path = realty_database_path
         self.price_database_path = price_database_path
+        self.latest_update_path = latest_update_path
 
         # address to fetch price data for
         self.address_list = self.load_realty_address()
@@ -59,9 +61,16 @@ class DataBuilder:
                 f.write(data_json)
                 f.write('\n')
 
+    def update_lastest_update_file(self):
+        with open(self.latest_update_path, 'w+', encoding='utf-8') as f:
+            f.write('lastest update:\n')
+            f.write(convert_timestamp_to_readable_format(self.timestamp))
+
     def run(self):
         self.request_price_data()
         self.save_price_data()
+        if self.latest_update_path:
+            self.update_lastest_update_file()
 
 
 if __name__ == '__main__':
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 
 def get_project_root():
@@ -7,3 +8,9 @@ def get_project_root():
 
 def get_src_root():
     return os.path.dirname(os.path.abspath(__file__)) + '/'
+
+
+def convert_timestamp_to_readable_format(timestamp: int):
+    datetime_obj = datetime.datetime.fromtimestamp(timestamp)
+    readable_format = datetime_obj.strftime('%Y-%m-%d %H:%M:%S')
+    return readable_format
